c22e1497bbd092b786a4c8afb974a0a0dee78b82
Hrishikesh Kulkarni <hrishikeshkulkarni153@gmail.com>
2026-05-31 23:20:10 -0400

    This is what I learnt part 4

---
+++ src/sandbox.ts
@@ -0,0 +1,34 @@
+// //We can use any as type if we do not have a fixed type decided. But it is dangerous to use it sometimes
+
+// let age:any = 20
+
+// age = 'Emma'
+// age = true
+
+// let mixed: any[] = []
+
+// mixed.push(30)
+// mixed.push('Ellie')
+// mixed.push(false)
+
+// let ninja : {
+//     name: any,
+//     age: any
+// };
+
+// ninja = {name:'shiro', age:30} // Allowed
+
+// ninja = {name:30, age:'misa'} // Also allowed
+
+let greet: Function;
+
+greet = () => {
+    console.log("Hello")
+}
+
+const add = (a:number, b:number, c?:number|string) => { // ?-shows optional param
+    console.log(a + b);
+    console.log(c); //should show undefined if 3rd parameter is not sent in the func call
+}
+
+add(5,10);
