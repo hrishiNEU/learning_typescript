72db7b61770a995fbccd2ef8fc6cc417dc0dc9df
Hrishikesh Kulkarni <hrishikeshkulkarni153@gmail.com>
2026-06-01 21:40:16 -0400

    This is what I learnt in part 8 - Interfaces, classes and rendering HTML template

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,17 +1,6 @@
-// Classes 
-import { Invoice } from './classes/invoice'
-
-let invOne = new Invoice('Chun','Web development',50);
-let invTwo = new Invoice('Shin','System design',70);
-
-let invoices : Invoice[] = []
-
-invoices.push(invOne);
-invoices.push(invTwo);
-
-invoices.forEach(inv => {
-    console.log(inv.client,inv.amount, inv.format()) 
-})
+import { Invoice } from './classes/Invoice.js';
+import { Payment } from './classes/Payment.js';
+import { HasFormatter } from './interfaces/HasFormatter.js';
 
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
 
@@ -22,6 +11,15 @@ const amount = document.querySelector('#amount') as HTMLInputElement;
 
 form.addEventListener('submit', (e:Event) => {
     e.preventDefault();
+
+    let doc: HasFormatter;
+
+    if(type.value === 'invoice'){
+        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber)
+    }else{
+        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber)
+    }
+
     console.log(
         type.value,
         tofrom.value,
@@ -29,3 +27,52 @@ form.addEventListener('submit', (e:Event) => {
         amount.valueAsNumber
     );
 })
+
+
+// Interfaces
+// interface isPerson{
+//     name: string;
+//     age: number;
+//     speak(a:string): void;
+//     spend(a:number):number;
+// }
+
+// const me: isPerson = {
+//     name: 'Harry',
+//     age: 30,
+//     speak(text:string){
+//         console.log(text)
+//     },
+//     spend(money:number){
+//         console.log('I spend ',money)
+//         return money
+//     }
+// }
+
+// const greetPerson = (person: isPerson) => {
+//     console.log('Hello',person.name)
+// }
+
+// let docOne : HasFormatter;
+// let docTwo : HasFormatter;
+
+// docOne = new Invoice('Lin','UI UX',60);
+// docTwo = new Invoice('Dan','Infra setup',80);
+
+// let docs: HasFormatter[] = []
+// docs.push(docOne)
+// docs.push(docTwo)
+
+// console.log(docs)
+
+// let invOne = new Invoice('Chun','Web development',50);
+// let invTwo = new Invoice('Shin','System design',70);
+
+// let invoices : Invoice[] = []
+
+// invoices.push(invOne);
+// invoices.push(invTwo);
+
+// invoices.forEach(inv => {
+//     console.log(inv.client,inv.amount, inv.format()) 
+// })
