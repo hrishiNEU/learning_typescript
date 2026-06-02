bdfbbafe7f31babaa19b1d56d78201c69afb98d0
Hrishikesh Kulkarni <hrishikeshkulkarni153@gmail.com>
2026-06-01 22:10:45 -0400

    Learnt generics, enums and tuples

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -5,8 +5,8 @@ import { HasFormatter } from './interfaces/HasFormatter.js';
 
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
 
-//inputs
-const type = document.querySelector('#type') as HTMLSelectElement;
+// inputs
+const type = document.querySelector('#type') as HTMLInputElement;
 const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
 const details = document.querySelector('#details') as HTMLInputElement;
 const amount = document.querySelector('#amount') as HTMLInputElement;
@@ -15,20 +15,35 @@ const amount = document.querySelector('#amount') as HTMLInputElement;
 const ul = document.querySelector('ul')!;
 const list = new ListTemplate(ul);
 
-form.addEventListener('submit', (e:Event) => {
-    e.preventDefault();
+form.addEventListener('submit', (e: Event) => {
+  e.preventDefault();
 
-    let doc: HasFormatter;
+  let values: [string, string, number];
+  values = [tofrom.value, details.value, amount.valueAsNumber];
 
-    if(type.value === 'invoice'){
-        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber)
-    }else{
-        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber)
-    }
+  let doc: HasFormatter;
+  if (type.value === 'invoice') {
+    doc = new Invoice(...values);
+  } else {
+    doc = new Payment(...values);
+  }
+  
+  list.render(doc, type.value, 'end');
+});
 
-    list.render(doc, type.value, 'end');
-})
+// TUPLES
+let arr = ['ryu', 25, true];
+arr[0] = false;
+arr[1] = 'yoshi';
+arr = [30, false, 'yoshi'];
 
+let tup: [string, number, boolean] = ['ryu', 25, true];
+// tup[0] = false;
+tup[0] = 'ken';
+
+let student: [string, number];
+//student = [23564, 'chun-li'];
+student = ['chun-li', 23564];
 
 // Interfaces
 // interface isPerson{
