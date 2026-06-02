15220532136ac226fe6f608bc450798350ec36ab
Hrishikesh Kulkarni <hrishikeshkulkarni153@gmail.com>
2026-06-01 21:48:33 -0400

    I learnt more about HTMLTemplate render

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,10 +1,15 @@
 import { Invoice } from './classes/Invoice.js';
 import { Payment } from './classes/Payment.js';
+import { ListTemplate } from './classes/ListTemplate.js';
 const form = document.querySelector('.new-item-form');
+//inputs
 const type = document.querySelector('#type');
 const tofrom = document.querySelector('#tofrom');
 const details = document.querySelector('#details');
 const amount = document.querySelector('#amount');
+// list template instance
+const ul = document.querySelector('ul');
+const list = new ListTemplate(ul);
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     let doc;
@@ -14,7 +19,7 @@ form.addEventListener('submit', (e) => {
     else {
         doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
     }
-    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+    list.render(doc, type.value, 'end');
 });
 // Interfaces
 // interface isPerson{
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,13 +1,19 @@
 import { Invoice } from './classes/Invoice.js';
 import { Payment } from './classes/Payment.js';
+import { ListTemplate } from './classes/ListTemplate.js';
 import { HasFormatter } from './interfaces/HasFormatter.js';
 
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
 
+//inputs
 const type = document.querySelector('#type') as HTMLSelectElement;
 const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
 const details = document.querySelector('#details') as HTMLInputElement;
 const amount = document.querySelector('#amount') as HTMLInputElement;
+
+// list template instance
+const ul = document.querySelector('ul')!;
+const list = new ListTemplate(ul);
 
 form.addEventListener('submit', (e:Event) => {
     e.preventDefault();
@@ -20,12 +26,7 @@ form.addEventListener('submit', (e:Event) => {
         doc = new Payment(tofrom.value, details.value, amount.valueAsNumber)
     }
 
-    console.log(
-        type.value,
-        tofrom.value,
-        details.value,
-        amount.valueAsNumber
-    );
+    list.render(doc, type.value, 'end');
 })
 
 
